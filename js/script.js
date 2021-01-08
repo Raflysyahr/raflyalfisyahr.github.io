de1f257270557a34e24a6d36c6b859087de9ee77
raflyalfisyahr <raflyalfisyahr@gmail.com>
2021-01-08 14:47:57 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const removeChar = str => str.split('');
+//     console.log(removeChar('eloquent'),'loquen');
+
+// const makeNegative = num => num > 0?-num:num;
+// console.log(makeNegative(42),-42);
+
+// const repeatStr = (n,s) => s.repeat(n);
+// console.log(repeatStr(1,'*'));
+// console.log(repeatStr(3,'*'));
+// console.log(repeatStr(6,'*'));
+// console.log(repeatStr(9,'*'));
